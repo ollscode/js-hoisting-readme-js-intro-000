d6e33bf470c4a87e32bfb9f6a90654f4bd81ba11
Oscar <mundonatural@gmail.com>
2020-01-27 14:47:44 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -3,18 +3,19 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  lyric = "maybe";
   return lyric;
-  var lyric = "maybe";
+  var lyric;
 }
 
 
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
+  // fix the code in here:
+  thisIsCrazy();
 }
 
 function sayMyName() {
@@ -24,7 +25,8 @@ function sayMyName() {
   sayMy();
 
   function sayMy() {
+    name = "Kristin";
     console.log(name);
-    var name = "Kristin";
+    var name;
   }
 }
